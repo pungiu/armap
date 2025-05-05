632ec0bb27f3eacb65dc59592e80df212d133377
Pungi <131058916+pungiu@users.noreply.github.com>
2025-05-05 13:45:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require_once 'db.php';
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -33,15 +37,32 @@
             <a-camera gps-camera rotation-reader> </a-camera>
         </a-scene>
 
-        <div class="map-container">
+        <div class="map-container" id="map-container">
             <div id="map"></div>
         </div>
 
-        <div class="dock">
-            <div cass="">&cross;</div>
-            <div cass="">&cross;</div>
-            <div cass="">&cross;</div>
+        <div class="moznosti" id="moda">
+            <div id="login">
+                <div id="login-container">
+                    <form action="login.php" method="post" class="hdx">
+                        <label for="regin">Vnesi uporabnisko ime</label>
+                        <input type="text" placeholder="Uporabnisko ime" name="username">
+                        <label for="regin">Vnesi geslo</label>
+                        <input type="password" placeholder="Geslo" name="geslo">
+                        <input type="text" name="email" placeholder="Vnesi email za registracijo" id="skrit">
+                        <input type="submit" value="prijava" id="submitgumb">
+                    </form>
+                    <div onclick="regi()">Registracija</div>
+                </div>
+            </div>
         </div>
+
+        <div class="dock">
+            <div class="main-icon" id="icon1" onclick="gumbi(this)"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/></svg></div>
+            <div class="main-icon" id="icon2" onclick="gumbi(this)">b</div>
+            <div class="main-icon" id="icon3" onclick="gumbi(this)">c</div>
+        </div>
+        <?php var_dump($_SESSION); ?>
     </div>
 
     <div id="status">Initializing...</div>
